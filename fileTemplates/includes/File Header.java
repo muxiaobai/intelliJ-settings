a336f7ffa32dc0f3d08fe99496fb42157c84fbb9
zhangpengfei <zpf12345678910@gmail.com>
2020-09-24 19:45:10 +0800

    IU-2019.1.2 <Administrator@muxiaobai Create markdown.xml, p3c.xml, project.default.xml, diff.xml, Default copy.xml, yiiguxing.translation.xml, Default.xml, find.xml, File Header.java, keymap.xml, KeyPromoterXSettings.xml, _@user_Darcula.icls, mavenVersion.xml, GrepConsole.xml, abbreviations.xml, Default.xml, editor.codeinsight.xml, Mybatis_SQL.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, ide-features-trainer.xml, lombok-plugin.xml, terminal.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, Java.xml, highlight-bracket-pair-plugin.xml, plugin_ui.xml, colors.scheme.xml, VisualVMLauncher.xml, databaseDrivers.xml, templates.xml, stringManipulation.xml, consoleFolding.xml, customization.xml, jprofiler.xml, laf.xml, jdk.table.xml, path.macros.xml, proxy.settings.xml, vcs.xml, codota.xml, databaseSettings.xml, mybatis.xml, usageView.xml, sonarlint.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+/**
+* @PROJECT_NAME ${PROJECT_NAME}
+* @Package ${PACKAGE_NAME}
+* @author Zhang Pengfei
+* @date ${DATE} ${TIME}
+* @TODO   TODO
+* @version V1.0
+* @Copyright © 2000-2019 Xi'an Webber Software. All Rights Reserved.
+*/
